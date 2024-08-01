33d0d154cad4aa78952379595706e4c1f4e3af10
ssamiaa <samiasajid2@gmail.com>
2024-08-01 14:15:24 +0530

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,29 @@
+import './style.scss'
+
+let curX : number = 0;
+let curY : number = 0;
+let tgX : number = 0;
+let tgY : number = 0;
+
+
+document.addEventListener('DOMContentLoaded', () : void => {
+    const interBubble: HTMLDivElement = document.querySelector<HTMLDivElement>('.interactive')!;
+
+
+    function move(): void {
+        curX += (tgX - curX) / 20;
+        curY += (tgY - curY) / 20;
+        interBubble.style.transform = `translate(${Math.round(curX)}px, ${Math.round(curY)}px)`;
+        requestAnimationFrame(() : void => {
+            move();
+
+        });
+    }
+    window.addEventListener('mousemove', (event) => {
+        tgX = event.clientX;
+        tgY = event.clientY;
+    });
+
+    move();
+
+});
